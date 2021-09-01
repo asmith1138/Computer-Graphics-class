3c6c0c25c132b4c66041b4c68c0d33379588f034
Andrew Smith <asmith1138@gmail.com>
2021-09-01 09:47:52 -0400

    Added answer

--- hw1/hw1.cxx
+++ hw1/hw1.cxx
@@ -6,13 +6,38 @@
  *  azs7014                                                          *
  *********************************************************************/  
 
-/*********************************************************************
- *                                                                   *
- *                                                                   *
- *     Put your answer to the written part of assignment here        *
- *                                                                   *
- *                                                                   *
- *********************************************************************/
+/*************************************************************************
+ * First, I will explain this part ((w & 0x08) == 0) ^ ((h & 0x08) == 0);*
+ * This takes the column and uses bitwise AND with 0x08(hex value) to    *
+ * transform it, since 0x08 is binary 1000 only w values with a 1 in the *
+ * 8s place will result in a value of 8, otherwise it will be 0, the h   *
+ * or row value will have the same effect in its own bitwise operation.  *
+ * This means that every 8 values will switch from a result of 8 and a   *
+ * result of 0, 0 results will return true due to the ==0 and 8 will be  *
+ * false.  Finally, the ^ or XOR will return a true if only 1 of the     *
+ * column or row values results in a 1 in the 8s place.  This gives a    *
+ * checkerboard pattern of size 8 pixels.  To change this we can change  *
+ * the 0x08 to another number with a binary representation that only has *
+ * one 1 in it.  Such as 0x04 for checkerboard spaces half the size or   *
+ * 0x10 for twice the size, other options(0x02,0x20,0x40,etc.).          *
+ * 
+ * Second, the static casts to float of col.  At this point col is       *
+ * either 1 or 0.  A bitwise AND will compare 0x00 or 0x11 to 1 or 0,    *
+ * this comes to 00001 & 00000 or 00001 & 10001. This means the 1 value  *
+ * always returns 1 for both r(since no operation is done) and b(since   *
+ * 00001 and 10001 both have a 1 in the 1s place), while g always is     *
+ * 0 because of the 00000 value. The 0 value for col always continues to *
+ * be 0, this means the checkerboard is black(0,0,0) or magenta(1,0,1).  *
+ * To change this we would need to change the bitwise & values in the    *
+ * static cast, but since we are starting with a 1 or 0 we don't have a  *
+ * way to set rgb values other than 0 or 255.  Since we will be          *
+ * multiplying the resulting 1 or 0 with 255 prior to writing to the ppm *
+ * file.
+ * 
+ * We multiply the 1 or 0 in the raster by 255 so that the ppm will      *
+ * show the full color or not. The raster holds values between 1 and 0,  *
+ * while the ppm file uses 0 to 255 to convey the same information.      *
+ ************************************************************************/
 
 
 
